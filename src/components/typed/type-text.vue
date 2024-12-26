43c23065f2446fe3ec64aea2b869dd2d59fb8a03
zhangcong <1152818861@qq.com>
2024-12-26 19:04:31 +0800

    feat 首页打字效果更换实现方式

--- src/components/typed/type-text.vue
+++ src/components/typed/type-text.vue
@@ -1,43 +1,70 @@
 <template>
   <div class="text-box">
-    <span id="type-list" class="text"></span>
-    <span class="line">|</span>
+    <span
+      id="type-list"
+      class="text"
+    ></span>
+    <span
+      class="line"
+      :class="{ blink: blinkLine }"
+      >|</span
+    >
   </div>
 </template>
 
 <script setup>
-import { defineProps, watch } from "vue";
+import { ref, watch } from "vue";
 const props = defineProps(["text"]);
 watch(
   () => props.text,
-  () => {
-    const container = document.getElementById("type-list");
-    const task = (i) => {
-      let width = 0;
-      const increase = setInterval(() => {
-        width += 2;
-        container.innerHTML = props.text[i];
-        container.style.width = width + "rem";
-        if (width >= props.text[i].length * 2) {
-          clearInterval(increase);
-          const decrease = setInterval(() => {
-            width -= 2;
-            container.style.width = width + "rem";
-            if (width <= 0) {
-              clearInterval(decrease);
+
+  async () => {
+    for (const item of props.text) {
+      const arr = item.split("");
+      // console.log(stringArr);
+      const type = new Promise((resolve, reject) => {
+        const container = document.getElementById("type-list");
+        //设置一个定时器
+        let i = 0;
+        //等待1s后持续添加文字
+        setTimeout(() => {
+          blinkLine.value = false;
+          const timer = setInterval(() => {
+            if (i < arr.length) {
+              // console.log(container.innerHTML);
+              container.innerHTML += arr[i];
               i++;
-              if (i > props.text.length - 1) {
-                i = 0;
-              }
-              task(i);
             }
-          }, props.text[i].length * 10);
-        }
-      }, props.text[i].length * 20);
-    };
-    task(0);
+            //如果添加完成
+            else {
+              clearInterval(timer);
+              //等待1s后持续删除文字
+              // console.log(container.innerHTML);
+              blinkLine.value = true;
+              setTimeout(() => {
+                blinkLine.value = false;
+                // console.log(i);
+                const timer = setInterval(() => {
+                  // console.log(container.innerHTML, i);
+                  if (i > 0) {
+                    container.innerHTML = container.innerHTML.slice(0, i - 1);
+                    i--;
+                  } else {
+                    clearInterval(timer);
+                    blinkLine.value = true;
+                    resolve();
+                  }
+                }, 90);
+              }, 1500);
+            }
+          }, 350);
+        }, 1500);
+      });
+      await type;
+    }
   }
 );
+const blinkLine = ref("false");
 </script>
 
 <style lang="scss" scoped>
@@ -48,18 +75,33 @@ watch(
   // display: flex;
   .text {
     display: inline-block;
-    width: 0;
+    // width: 0;
     height: 2rem;
     white-space: nowrap;
     font-size: 2rem;
     overflow: hidden;
   }
+  @keyframes blink {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
   .line {
+    font-family: Arial, Helvetica, sans-serif;
     display: inline-block;
     height: 2rem;
     line-height: 2rem;
-    font-size: 2rem;
+    font-size: 40px;
     vertical-align: top;
+  }
+  .blink {
+    animation: blink 0.75s ease-in-out 0s infinite;
   }
 }
 </style>
