65b58948bd20daed8c82484bc28a86cd84e39d84
zhangcong <1152818861@qq.com>
2025-09-06 17:02:19 +0800

    build webpack配置修改

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -5,6 +5,19 @@ const { VueLoaderPlugin } = require('vue-loader');
 const ESlintPlugin = require('eslint-webpack-plugin');
 const path = require('path');
 
+function getStyleLoaders(type = 'css') {
+  const loaders = [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    'postcss-loader'
+  ];
+  if (type === 'sass') {
+    loaders.push('sass-loader');
+  } else if (type === 'less') {
+    loaders.push('less-loader');
+  }
+  return loaders;
+}
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -28,74 +41,70 @@ module.exports = {
         loader: 'vue-loader',
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: [
-              '@babel/plugin-transform-runtime'
-            ]
+        // 每个文件只匹配其中一个规则，提升构建速度
+        oneOf: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true, // 开启babel编译缓存
+              cacheCompression: false // 关闭缓存文件压缩
+            }
+            // use: {
+            //   loader: 'babel-loader',
+            //   options: {
+            //     presets: ['@babel/preset-env'],
+            //     plugins: [
+            //       '@babel/plugin-transform-runtime'
+            //     ]
+            //   },
+            // },
           },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader'
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.less$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'less-loader',
-        ],
-      },
-      // 处理图片和字体
-      // {
-      //   test: /\.(png|jpg|gif|svg|woff2?|ttf)$/, // 匹配图片和字体
-      //   use: {
-      //     loader: 'url-loader',
-      //     options: {
-      //       limit: 8192, // 小于 8KB 的文件转为 base64
-      //       name: 'assets/[name].[hash:8].[ext]' // 大于 8KB 的文件输出路径
-      //     }
-      //   },
-      // },
-      // 图片配置
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        type: 'asset',
-        parser: {
-          dataUrlCondition: { maxSize: 8 * 1024 } // 图片小文件转base64
-        },
-        generator: {
-          filename: 'imgs/[contenthash][ext]'
-        },
-        // use: 'image-webpack-loader' // 对图片压缩
-      },
-      // 字体配置
-      {
-        test: /\.(woff2?|ttf|eot)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext]' // 字体使用原名，便于引用
-        }
+          {
+            test: /\.css$/,
+            use: getStyleLoaders('css'),
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: getStyleLoaders('sass'),
+          },
+          {
+            test: /\.less$/i,
+            use: getStyleLoaders('less'),
+          },
+          // 处理图片和字体
+          // {
+          //   test: /\.(png|jpg|gif|svg|woff2?|ttf)$/, // 匹配图片和字体
+          //   use: {
+          //     loader: 'url-loader',
+          //     options: {
+          //       limit: 8192, // 小于 8KB 的文件转为 base64
+          //       name: 'assets/[name].[hash:8].[ext]' // 大于 8KB 的文件输出路径
+          //     }
+          //   },
+          // },
+          // 图片配置
+          {
+            test: /\.(png|jpg|gif|svg)$/,
+            type: 'asset',
+            parser: {
+              dataUrlCondition: { maxSize: 8 * 1024 } // 图片小文件转base64
+            },
+            generator: {
+              filename: 'imgs/[contenthash][ext]'
+            },
+            // use: 'image-webpack-loader' // 对图片压缩
+          },
+          // 字体配置
+          {
+            test: /\.(woff2?|ttf|eot)$/,
+            type: 'asset/resource',
+            generator: {
+              filename: 'fonts/[name][ext]' // 字体使用原名，便于引用
+            }
+          }
+        ]
       }
     ],
   },
@@ -111,16 +120,22 @@ module.exports = {
     }),
     new VueLoaderPlugin(),  // 解析 .vue 文件必需
     new ESlintPlugin({  // 代码检查
+      context: path.resolve(__dirname, '../src'),
       extensions: ['js', 'vue'],
       exclude: 'node_modules',
-      emitWarning: true, // 将错误作为警告输出，不会中断编译
+      cache: true, // 开启缓存
+      cacheLocation: path.resolve(__dirname, '../node_modules/.cache/eslintcache'), // 缓存目录
+      // threads: true // 开启多线程
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].[contenthash:8].css', // 输出 CSS 文件到 styles 目录
     }),
     new DefinePlugin({  //定义全局变量和环境变量
       // 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'BASE_URL': JSON.stringify('/') // 自定义全局变量
+      BASE_URL: JSON.stringify('/'), // 自定义全局变量
+      __VUE_OPTIONS_API__: JSON.stringify(false),
+      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
     })
   ],
   optimization: {
--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -10,6 +10,10 @@ module.exports = {
     open: true,
     hot: true,
     historyApiFallback: true,
+    client: {
+      overlay: false, // 取消webpack编译错误时，浏览器全屏提示错误
+      logging: 'info' // 控制台输出信息级别
+    },
     proxy: [
       {
         context: '/web',
@@ -28,5 +32,16 @@ module.exports = {
         secure: false
       }
     ]
+  },
+  stats: {
+    // 控制控制台输出的信息详细程度
+    errors: true,        // 显示错误信息（默认true）
+    warnings: true,      // 显示警告信息（默认true）
+    errorDetails: true,  // 显示错误的详细信息（如错误位置、堆栈）
+    moduleTrace: true,   // 显示错误的模块追踪信息
+    all: false           // 关闭所有设置为false，只显示上面开启的信息
+  },
+  performance: {
+    hints: false,        // 关闭性能相关的提示（如文件过大警告）
   },
 }
